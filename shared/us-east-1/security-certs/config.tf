1b95b5cdbc9304df0e160dfdfbf30e53fe1898ae
Esteban Pertile <esteban.pertile@binbash.com.ar>
2022-05-03 14:36:45 -0300

    add cert layer

---
+++ shared/us-east-1/security-certs/config.tf
@@ -0,0 +1,41 @@
+#=============================#
+# AWS Provider Settings       #
+#=============================#
+provider "aws" {
+  region                  = var.region
+  profile                 = var.profile
+  shared_credentials_file = "~/.aws/${var.project}/config"
+}
+
+#=============================#
+# Backend Config (partial)    #
+#=============================#
+terraform {
+  required_version = ">= 0.14.11"
+
+  required_providers {
+    aws = "~> 3.0"
+  }
+
+  backend "s3" {
+    key = "shared/security-certs/terraform.tfstate"
+  }
+}
+
+#=============================#
+# Data sources                #
+#=============================#
+
+#
+# data type from output for dns
+#
+data "terraform_remote_state" "dns" {
+  backend = "s3"
+
+  config = {
+    region  = var.region
+    profile = "${var.project}-shared-devops"
+    bucket  = "${var.project}-shared-terraform-backend"
+    key     = "${var.environment}/dns/binbash.com.ar/terraform.tfstate"
+  }
+}
